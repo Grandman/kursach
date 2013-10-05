f7b8d8345ffdb47c07259332e4f5a45397636a92
Grandman73 <root@grandman73.ru>
2013-10-05 09:32:33 +0400

    Completed DB class

--- KursachV3/DB.cs
+++ KursachV3/DB.cs
@@ -9,92 +9,62 @@ namespace KursachV3
 {
     static class Db
     {
-        static readonly SqlConnection Sqlc = new SqlConnection(Properties.Settings.Default.Database1ConnectionString);
+        static readonly string ConnectionString = Properties.Settings.Default.Database1ConnectionString;
 
-        static bool Connect()
+        public static DataTable Select(string cols, string table, string sortColumn = "", string sortDestination = "", string where = "")
         {
-            try
+            using (SqlConnection sqlc = new SqlConnection(ConnectionString))
             {
-                Sqlc.Open();
-                return true;
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(string.Format("Ошибка подключения:\n{0}", ex.Message));
-                return false;
-            }
-
-        }
-
-        static void Disconnect()
-        {
-            try
-            {
-                Sqlc.Close();
-            }
-            catch (SqlException exception)
-            {
-                MessageBox.Show(string.Format("Ошибка закрытия подключения\n{0}", exception.Message));
-            }
-        }
-        public static DataTable Select(string cols, string table, string sortColumn = "", string sortDestination = "",string where = "")
-        {
-            if (!Connect())
-            {
-                return null;
-            }
-            var dtable = new DataTable();
-            var query = "SELECT " + cols + " FROM " + table;
-            if (@where != "")
-            {
-                query += " WHERE " + @where;
-            }
-            if (sortColumn != "")
-            {
-                query += " order by " + sortColumn;
-            }
-            if (sortDestination != "")
-            {
-                query += " " + sortDestination;
-            }
-            var command = Sqlc.CreateCommand();
-            command.CommandText = query;
-            try
-            {
+                var dtable = new DataTable();
+                var query = "SELECT " + cols + " FROM " + table;
+                if (@where != "")
+                {
+                    query += " WHERE " + @where;
+                }
+                if (sortColumn != "")
+                {
+                    query += " order by " + sortColumn;
+                }
+                if (sortDestination != "")
+                {
+                    query += " " + sortDestination;
+                }
+                var command = sqlc.CreateCommand();
+                command.CommandText = query;
                 var dataAdapter = new SqlDataAdapter(command);
                 dataAdapter.Fill(dtable);
+                return dtable;
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                return null;
-            }
-            Disconnect();
-            return dtable;
         }
 
         public static bool Update(string table, Dictionary<string,object> update, int id = 0)
         {
-            var command = new SqlCommand("UPDATE " + table + " SET ", Sqlc);
-            command.CommandText += String.Join(",", update.Select(elem => elem.Key + "=@" + elem.Key));
-            command = SetValues(update,command);
-            if (id != 0)
+            using (SqlConnection sqlc = new SqlConnection(ConnectionString))
             {
-                command.CommandText += " WHERE id=" + id;
+                var command = new SqlCommand("UPDATE " + table + " SET ", sqlc);
+                command.CommandText += String.Join(",", update.Select(elem => elem.Key + "=@" + elem.Key));
+                command = SetValues(update, command);
+                if (id != 0)
+                {
+                    command.CommandText += " WHERE id=" + id;
+                }
+                MessageBox.Show(command.CommandText);
+                return Exec(command);
             }
-            MessageBox.Show(command.CommandText);
-            return Exec(command);    
         }
         public static bool Insert(string table, Dictionary<string,object> vars)
         {
-            var sqlcom = Sqlc.CreateCommand();
-            sqlcom.CommandText = "INSERT INTO " + table + " ";
-            sqlcom.CommandText += "(" + String.Join(",", vars.Select(elem => elem.Key)) + ")";
-            sqlcom.CommandText +=" VALUES ("; 
-            sqlcom.CommandText += String.Join(",", vars.Select(elem => "@" + elem.Key));
-            sqlcom = SetValues(vars, sqlcom);
-            sqlcom.CommandText += ")";
-            return Exec(sqlcom);
+            using (SqlConnection sqlc = new SqlConnection(ConnectionString))
+            {
+                var sqlcom = sqlc.CreateCommand();
+                sqlcom.CommandText = "INSERT INTO " + table + " ";
+                sqlcom.CommandText += "(" + String.Join(",", vars.Select(elem => elem.Key)) + ")";
+                sqlcom.CommandText += " VALUES (";
+                sqlcom.CommandText += String.Join(",", vars.Select(elem => "@" + elem.Key));
+                sqlcom = SetValues(vars, sqlcom);
+                sqlcom.CommandText += ")";
+                return Exec(sqlcom);
+            }
         }
 
         private static SqlCommand SetValues(Dictionary<string, object> vars, SqlCommand sqlcom)
@@ -108,29 +78,17 @@ namespace KursachV3
 
         static bool Exec(SqlCommand command)
         {
-            if (Connect())
-            {
-                bool result = true;
-                try
-                {
-                    command.ExecuteNonQuery();
-                }
-                catch (SqlException ex)
-                {
-                    MessageBox.Show(string.Format("Что-то пошло не так:\n{0}", ex.Message));
-                    result = false;
-                }
-                Disconnect();
-                return result;
-            }
-            return false;
+                return command.ExecuteNonQuery() >= 1;
         }
 
         public static bool Delete(string table, int id)
         {
-                var sqlcom = Sqlc.CreateCommand();
-                sqlcom.CommandText = "DELETE FROM " + table + " WHERE id =" + id;
+            using (SqlConnection sqlc = new SqlConnection(ConnectionString))
+            {
+                var sqlcom = sqlc.CreateCommand();
+                sqlcom.CommandText = "DELETE FROM " + table + " WHERE id=" + id;
                 return Exec(sqlcom);
+            }
         }
     }
 }
