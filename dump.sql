9174be4267f22aaeb2287dcce3dada3dfd3f4e76
Grandman73 <root@grandman73.ru>
2014-09-05 10:13:53 +0400

    added new tables

--- dump.sql
+++ dump.sql
@@ -1,19 +1,32 @@
-CREATE TABLE [dbo].[articles] (
-    [id]   INT  IDENTITY (1, 1) NOT NULL,
-    [size] INT  NOT NULL,
-    [date] DATE NOT NULL,
-    [type] INT  NOT NULL,
-    [kind] INT  NOT NULL,
-    [name] NVARCHAR(50) NOT NULL, 
+CREATE TABLE months_records
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [limit] INT NOT NULL, 
+    [size] INT NOT NULL, 
+    [diff] INT NOT NULL, 
+    [month] DATE NOT NULL
+)
+CREATE TABLE accounting (
+    [Id]         INT  IDENTITY (1, 1) NOT NULL,
+    [article_id] INT  NOT NULL,
+    [date]       DATE NOT NULL,
+    [size]       INT  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+CREATE TABLE articles (
+    [id]    INT           IDENTITY (1, 1) NOT NULL,
+    [type]  INT           NOT NULL,
+    [limit] INT           NOT NULL,
+    [name]  NVARCHAR (50) NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
-CREATE TABLE [dbo].[client_tour] (
+CREATE TABLE client_tour (
     [id]        INT IDENTITY (1, 1) NOT NULL,
     [tour_id]   INT NOT NULL,
     [client_id] INT NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
-CREATE TABLE [dbo].[clients] (
+CREATE TABLE clients (
     [id]     INT        IDENTITY (1, 1) NOT NULL,
     [fio]    NCHAR (50) NOT NULL,
     [phone]  INT        NOT NULL,
@@ -22,7 +35,7 @@ CREATE TABLE [dbo].[clients] (
     [min]    INT        NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
-CREATE TABLE [dbo].[tours] (
+CREATE TABLE tours (
     [id]          INT            IDENTITY (1, 1) NOT NULL,
     [price]       INT            NULL,
     [date]        DATE           NULL,
@@ -33,3 +46,8 @@ CREATE TABLE [dbo].[tours] (
     [country]     NVARCHAR (60)  NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
+
+
+
